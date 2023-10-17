2cd2e4ab8fe56e989132643f9e94d7977c5fb8d6
MrBY510KONFACEBOOK <134196987+MrBY510KONFACEBOOK@users.noreply.github.com>
2023-10-17 17:38:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,73 +1,64 @@
 const { Client } = require('whatsapp-web.js');
-const client = new Client();
 const axios = require('axios');
 const fs = require('fs');
 const qrCodeTerminal = require('qrcode-terminal');
 
-const adminNumber = '0706586102';
-
-// List to store group information
-const groupsInfo = [];
+const client = new Client();
 
 // Event handler for incoming messages
 client.on('message', async (message) => {
-  if (message.from.includes(adminNumber)) {
-    if (message.body === '!getgroups') {
-      // Retrieve group information
-      const groups = await client.getChats();
+  if (message.body === '!getgroups') {
+    // Retrieve group information
+    const groups = await client.getChats();
 
-      let response = 'Groups Information:\n';
+    let response = 'Groups Information:\n';
 
-      groups.forEach((group) => {
-        // Store group information
-        groupsInfo.push({ name: group.name, id: group.id._serialized });
+    groups.forEach((group) => {
+      // Add group information to the response
+      response += `${group.name}\n${group.id._serialized}\n\n`;
+    });
 
-        // Add group information to the response
-        response += `${group.name}\n${group.id._serialized}\n\n`;
-      });
+    message.reply(response);
+  } else if (message.body.startsWith('!sendmedia')) {
+    const args = message.body.split(' ');
+    const mediaUrl = args[1];
+    const targetGroupId = args[2]; // Add group ID as the third argument
 
-      message.reply(response);
-    } else if (message.body.startsWith('!sendmedia')) {
-      const args = message.body.split(' ');
-      const mediaUrl = args[1];
-      const targetGroupId = args[2]; // Add group ID as the third argument
+    // Download the file
+    const response = await axios({
+      method: 'GET',
+      url: mediaUrl,
+      responseType: 'stream',
+    });
 
-      // Download the file
-      const response = await axios({
-        method: 'GET',
-        url: mediaUrl,
-        responseType: 'stream',
-      });
+    // Save the file locally
+    const filePath = 'downloaded_file';
+    const writer = fs.createWriteStream(filePath);
+    response.data.pipe(writer);
 
-      // Save the file locally
-      const filePath = 'downloaded_file';
-      const writer = fs.createWriteStream(filePath);
-      response.data.pipe(writer);
+    // Wait for the file to be saved
+    await new Promise((resolve, reject) => {
+      writer.on('finish', resolve);
+      writer.on('error', reject);
+    });
 
-      // Wait for the file to be saved
-      await new Promise((resolve, reject) => {
-        writer.on('finish', resolve);
-        writer.on('error', reject);
-      });
+    // Send the saved file as a document to the specified group
+    const groupChat = client.getChatById(targetGroupId);
 
-      // Send the saved file as a document to the specified group
-      const groupChat = client.getChatById(targetGroupId);
-
-      if (groupChat) {
-        groupChat.sendDocument(fs.createReadStream(filePath), { filename: 'sent_document' })
-          .then(() => {
-            message.reply('Document sent successfully to the specified group.');
-          })
-          .catch((error) => {
-            message.reply(`Error sending document: ${error}`);
-          })
-          .finally(() => {
-            // Delete the temporary file after sending
-            fs.unlinkSync(filePath);
-          });
-      } else {
-        message.reply('Invalid group ID. Please check and try again.');
-      }
+    if (groupChat) {
+      groupChat.sendDocument(fs.createReadStream(filePath), { filename: 'sent_document' })
+        .then(() => {
+          message.reply('Document sent successfully to the specified group.');
+        })
+        .catch((error) => {
+          message.reply(`Error sending document: ${error}`);
+        })
+        .finally(() => {
+          // Delete the temporary file after sending
+          fs.unlinkSync(filePath);
+        });
+    } else {
+      message.reply('Invalid group ID. Please check and try again.');
     }
   }
 });
